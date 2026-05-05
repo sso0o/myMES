58a04bc3721384fe83963fcf63695895856c03c6
choesoyeon <ssssso.yyyy@gmail.com>
2026-05-05 17:55:58 +0900

    config disable

--- backend/src/main/java/com/mymes/backend/bom/config/BomIndexConfig.java
+++ backend/src/main/java/com/mymes/backend/bom/config/BomIndexConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,7 @@ import java.sql.SQLException;
 
 @Slf4j
 @Component
+@ConditionalOnProperty(prefix = "mymes.bom-migration", name = "enabled", havingValue = "true")
 @RequiredArgsConstructor
 public class BomIndexConfig implements ApplicationRunner {
 
